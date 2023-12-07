3be1a1b0cee6513b50061ce29ceebc0beebc035e
Fadli-IT-GNZ <95587275+Sudisf@users.noreply.github.com>
2023-12-07 12:13:26 -0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,31 @@
+const header = document.querySelector("header")
+
+window.addEventListener ("scroll", function() {
+    header.classList.toggle ("sticky", window.scrollY > 200)
+});
+
+let menu = document.querySelector('#menu-icon');
+let navlist = document.querySelector('.navlist');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navlist.classList.toggle('open');
+};
+
+menu.onclick = () => {
+    menu.classList.remove('bx-x');
+    navlist.classList.remove('open');
+};
+
+
+const sr = ScrollReveal ({
+    distance: '40px',
+    duration: 2050,
+    delay: 200,
+    reset: true
+});
+
+sr.reveal('.hero-text',{origin: 'top'});
+sr.reveal('.about-img, .service-item, .about-text',{origin: 'bottom'});
+sr.reveal('.about-text h2, .text-center',{origin: 'top'});
+sr.reveal('.end-section',{origin: 'top'});
